db982106571f28f517daac9a268bc6202a41cf3c
derakayla5 <derakayla6@gmail.com>
2024-03-21 23:22:44 +0800

    Mini Project 2

---
+++ main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed('.content h1', {
+        strings: ["Dera Kayla Khairani"],
+        typeSpeed: 50,
+        backSpeed: 50,
+        loop: true
+    });
+});
+
+
